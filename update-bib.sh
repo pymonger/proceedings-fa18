63e86b5261e0cbf32758a56cb16f3eea3ddb6371
Gregor von Laszewski <laszewski@gmail.com>
2018-10-14 14:00:54 -0400

    update scripts and metadata

---
+++ update-bib.sh
@@ -0,0 +1,7 @@
+cd fa18-423-08; git mv paper/references.bib paper/paper.bib; git commit -m "paper/paper.bib" paper/paper.bib; git push
+cd fa18-523-56; git mv paper/references.bib paper/paper.bib; git commit -m "paper/paper.bib" paper/paper.bib; git push
+cd fa18-523-61; git mv paper/references.bib paper/paper.bib; git commit -m "paper/paper.bib" paper/paper.bib; git push; cd ..
+cd fa18-523-63; git mv paper/references.bib paper/paper.bib; git commit -m "paper/paper.bib" paper/paper.bib; git push; cd ..
+cd fa18-523-80; git mv paper/references.bib paper/paper.bib; git commit -m "paper/paper.bib" paper/paper.bib; git push; cd ..
+cd fa18-523-83; git mv paper/references.bib paper/paper.bib; git commit -m "paper/paper.bib" paper/paper.bib; git push; cd ..
+cd fa18-523-85; git mv paper/references.bib paper/paper.bib; git commit -m "paper/paper.bib" paper/paper.bib; git push; cd ..
---
+++ update-readme.sh
@@ -0,0 +1,1 @@
+Write file: ~/github/cloudmesh-community/proceedings-fa18/update-readme.sh
